dd3b70685ac605ffac262c8c35e6b7ff1992ced0
marmeladywka <74825525+marmeladywka@users.noreply.github.com>
2021-04-20 10:49:40 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$('.header__burger').click(function(event) {
+		$('.header__burger,.header__menu').toggleClass('active');
+		$('body').toggleClass('lock');		
+	});
+	$('.header__link').click(function () {
+    $('.header__burger, .header__menu').removeClass('active');
+    $('body').removeClass('lock');
+});
+	
+});
